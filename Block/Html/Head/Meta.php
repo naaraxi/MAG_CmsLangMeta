0725ae43d1fc7a9b40ce819870218f160325e289
MAG <alex.mazalu@outlook.com>
2019-04-15 23:44:48 +0300

    Module skeleton

---
+++ Block/Html/Head/Meta.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * A Magento 2 module named MAG/CmsLangMeta
+ * Copyright (C) 2019 
+ * 
+ * This file included in MAG/CmsLangMeta is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace MAG\CmsLangMeta\Block\Html\Head;
+
+class Meta extends \Magento\Framework\View\Element\Template
+{
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function displayMetaTag()
+    {
+        //Your block code
+        return __('Hello Developer! This how to get the storename: %1 and this is the way to build a url: %2', $this->_storeManager->getStore()->getName(), $this->getUrl('contacts'));
+    }
+}
